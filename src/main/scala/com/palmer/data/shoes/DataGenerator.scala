b40b2aa2e0692a7c9a113f49c2703eec12ab3a34
Ross Palmer <rossbpalmer@gmail.com>
2022-11-13 11:57:31 -0700

    add additional methods to data generator

--- src/main/scala/com/palmer/data/shoes/DataGenerator.scala
+++ src/main/scala/com/palmer/data/shoes/DataGenerator.scala
@@ -1,8 +1,9 @@
 package com.palmer.data.shoes
 
+import java.sql.Date
 import scala.util.Random
 
-object GeneratorConfig {
+object RAND {
 
   val r = Random
   val BASE_PROBABILITIES = Seq(
@@ -19,29 +20,53 @@ object GeneratorConfig {
   val COLORS = Seq("red", "blue", "green", "yellow", "orange", "purple")
   def getRandomColor(): String = COLORS(r.nextInt(COLORS.size))
 
+  def getRandomString(n: Int): String = r.alphanumeric.take(n).mkString
+
+  val startTimestamp: Long = 1577904392000L // 2020-01-01
+  val endTimestamp: Long = 1672598792000L // 2023-01-01
+  val timestampDiff: Long = endTimestamp - startTimestamp
+
+  def getRandomDate(): Date = {
+    val randomDiff = r.nextLong(timestampDiff)
+    val randomTimestamp = startTimestamp + randomDiff
+    new Date(randomTimestamp)
+  }
+
 }
 
+case class CustomerInfo(customerId: Long, name: String)
 
 object DataGenerator extends App {
+
+
+  def generateCustomerInfo(customerId: Long): CustomerInfo= {
+    val firstNameLength = 3 + RAND.r.nextInt(10)
+    val lastNameLength = 3 + RAND.r.nextInt(10)
+    CustomerInfo(
+      customerId,
+      s"${RAND.getRandomString(firstNameLength)} ${RAND.getRandomString(lastNameLength)}"
+    )
+  }
 
   def generateShoeDescription(probabilityOverride: Map[String, Double] = Map.empty): String = {
 
     // Generate list of shapes by a random check
     // against the final shape probability
-    val shapes: Seq[String] = GeneratorConfig.BASE_PROBABILITIES
+    val shapes: Seq[String] = RAND.BASE_PROBABILITIES
       .map {
         case (shape, prob) =>
           val finalProb = probabilityOverride.getOrElse(shape, prob)
-          if (finalProb > GeneratorConfig.r.nextDouble()) Some(shape) else None
+          if (finalProb > RAND.r.nextDouble()) Some(shape) else None
       }
       .filter(_.isDefined)
       .map(_.get)
 
     // Reduce shape list and include random color description
-    if (shapes.isEmpty) GeneratorConfig.getRandomColor() else {
-      shapes.map(shape => s"${GeneratorConfig.getRandomColor()} $shape").mkString(" ")
+    if (shapes.isEmpty) RAND.getRandomColor() else {
+      shapes.map(shape => s"${RAND.getRandomColor()} $shape").mkString(" ")
     }
 
   }
+
 
 }
--- src/test/scala/com/palmer/data/shoes/DataGeneratorTest.scala
+++ src/test/scala/com/palmer/data/shoes/DataGeneratorTest.scala
@@ -5,7 +5,44 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class DataGeneratorTest extends AnyWordSpec with Matchers {
 
+  "RAND random generator" when {
+
+    "generating a date" should {
+
+      "create a date within 2020-01-01 and 2023-01-02" in {
+
+        val randomDate = RAND.getRandomDate()
+
+        // TODO add bounded assertion
+        println(randomDate)
+
+      }
+
+    }
+
+  }
+
   "DataGenerator" when {
+
+    "generating customer info" should {
+
+      "create a random string name" in {
+
+        val custId = 1020304L
+        val info = DataGenerator.generateCustomerInfo(custId)
+
+        info.customerId must equal (custId)
+
+        val nameParts = info.name.split("\\s")
+        nameParts must have length 2
+        nameParts(0).length must be >= 3
+        nameParts(1).length must be >= 3
+
+        println(s"Generated name: ${info.name}")
+
+      }
+
+    }
 
     "generating a shoe description" should {
 
